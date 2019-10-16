d0acabbfd8be7852254a43a6d480658d7577ef3d
beanearnia2003 <arnia.beane@aspirestudent.org>
2019-10-16 01:29:45 +0000

    I made a button wrk I hope!

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@ $( "document" ).ready(function() {
 
 
 
+    $("#button").click(function(){
+        // write code here
+        $(".output").text("boo");
+        let output=$(".input").val()
+    });
+
 });
+
+function checkVowel(str){
+  var firstLetterAy=str[0];
+  var vowels = ["a","e","i","o","u"];
+
+  if(vowels.includes(firstLetterAy)){
+    return "starts with consonant";
+  }
+}
